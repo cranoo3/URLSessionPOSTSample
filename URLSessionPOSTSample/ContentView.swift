fa214bb5a83d3205e02e12ddb86f5ab1c9fa1bcc
cranoo <75974473+cranoo3@users.noreply.github.com>
2024-03-30 01:48:21 +0900

    Initial Commit

---
+++ URLSessionPOSTSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSessionPOSTSample
+//
+//  Created by cranoo on 2024/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSessionPOSTSample/URLSessionPOSTSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionPOSTSampleApp.swift
+//  URLSessionPOSTSample
+//
+//  Created by cranoo on 2024/03/30.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionPOSTSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
