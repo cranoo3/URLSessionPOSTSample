65c964919282c53359e12a7890754b042df961e2
cranoo <75974473+cranoo3@users.noreply.github.com>
2024-03-30 01:58:04 +0900

    chore: gitignoreを適用するため、キャッシュを削除する

---
+++ URLSessionPOSTSample/APIClient.swift
@@ -0,0 +1,18 @@
+//
+//  APIClient.swift
+//  URLSessionPOSTSample
+//
+//  Created by cranoo on 2024/03/30.
+//
+
+import Foundation
+
+struct APIClient {
+    let url: String = ""
+    let apiKey: String = ""
+    
+    // MARK: 1. URLをString型からURL型に変換する
+    func makeURLComponent() -> URL {
+        
+    }
+}
